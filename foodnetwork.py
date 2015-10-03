c1775b81895bde4d29fb2ce639c3f3b0e55d34c4
Barnaby Colby <barnaby.colby@gmail.com>
2015-10-03 15:07:07 +0100

    Added pages for each of the target users.

--- foodnetwork.py
+++ foodnetwork.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/foodbank")
+def foodbank():
+    return render_template('foodbank.html')
+
+@app.route("/donator")
+def donator():
+    return render_template('donator.html')
+
+@app.route("/receiver")
+def receiver():
+    return render_template('receiver.html')
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
