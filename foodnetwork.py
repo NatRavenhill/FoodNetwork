15b016ae4ba5b29852738cb97cc42a14587c0aa1
NatalieRavenhill <natalie.ravenhill@googlemail.com>
2015-10-04 04:25:17 +0100

    added twilio texting

--- foodnetwork.py
+++ foodnetwork.py
@@ -3,9 +3,17 @@ from flask import render_template
 
 from login import LoginForm
 from registration import RegistrationForm
+from text import TextForm
+
+from twilio.rest import TwilioRestClient
 
 app = Flask(__name__)
 app.secret_key = 'gfdhajghjarejw ophyugipefqh984372824'
+
+#info for twilio
+account_sid = "ACabbdc6a9c7645d6a38069ac3fe6c99c5"
+auth_token = "04670510815d4503cdf5f8d5414bdb8a"
+client = TwilioRestClient(account_sid, auth_token)
 
 @app.route('/')
 def index():
@@ -20,7 +28,7 @@ def foodbank():
     if user:
         return render_template('foodbank.html', user=user)
     else:
-        return render_template('foodbankforms.html', registrationForm=registrationForm, loginForm=loginForm)
+        return render_template('foodbank.html', registrationForm=registrationForm, loginForm=loginForm )
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -48,6 +56,17 @@ def donator():
 @app.route('/receiver')
 def receiver():
     return render_template('esri.html')
+	
+
+@app.route('/foodbankText', methods=['GET', 'POST'])
+def text():
+    textForm = TextForm(request.form)
+    if request.method == 'POST' and  textForm.validate() :
+       message = client.messages.create(to="+447784456027", from_="+441922214241", body = textForm.foodbankName.data + " needs some more " + textForm.food.data)
+       return render_template('successText.html')
+    else:
+       return render_template('foodbankText.html', textForm = textForm)
+
 
 if __name__ == '__main__':
     app.debug = True
